1d077098cb80363986d6b9e0afbcacb93a0270ef
Kozlov Vadim <kozlovvr10@gmail.com>
2024-12-09 20:37:41 +0700

    Добавлены методы для рисования дерева. Добавлены методы для определения глубины.

--- GP.py
+++ GP.py
@@ -6,6 +6,12 @@ class Genetic_Node():
     parent = None
     dimension_count = None
 
+    def visualize(self, graph, parent_id=None):
+        node_id = str(id(self))
+        graph.node(node_id, self.__class__.__name__)
+        if parent_id:
+            graph.edge(parent_id, node_id)
+
 
 class Function(Genetic_Node):
     def calculate(self, x):
@@ -23,6 +29,14 @@ class unary_function(Function):
         self.node_sons['left'].define_parent([self,'left'])
     # def change_child(self,new_child):
     #     self.ref_in_parent = new_child
+    def get_depth(self):
+        return 1 + self.node_sons['left'].get_depth()
+    def visualize(self, graph, parent_id=None):
+        node_id = str(id(self))
+        graph.node(node_id, self.__class__.__name__)
+        if parent_id:
+            graph.edge(parent_id, node_id)
+        self.node_sons['left'].visualize(graph, node_id)
 
 class binary_function(Function):
     IsOperator = "binary_function"
@@ -36,7 +50,16 @@ class binary_function(Function):
         self.parent = reference
         self.node_sons['left'].define_parent([self,'left'])
         self.node_sons['right'].define_parent([self,'right'])
-
+    def get_depth(self):
+        return 1 + max(self.node_sons['left'].get_depth(), self.node_sons['right'].get_depth())
+    def visualize(self, graph, parent_id=None):
+        node_id = str(id(self))
+        graph.node(node_id, self.__class__.__name__)
+        if parent_id:
+            graph.edge(parent_id, node_id)
+        self.node_sons['left'].visualize(graph, node_id)
+        self.node_sons['right'].visualize(graph, node_id)
+    
 class GP_sin(unary_function):
     kind_function = "sin"
     def __init__(self):
@@ -114,6 +137,8 @@ class Terminal(Genetic_Node):
         nodes.append(self)
     def define_parent(self, reference):
         self.parent = reference
+    def get_depth(self):
+        return 1
 
     
 class Variable(Terminal):
@@ -122,6 +147,11 @@ class Variable(Terminal):
         self.IsTerminal = 'variable'
     def calculate(self, x):
         return x[:,self.var_index]
+    def visualize(self, graph, parent_id=None):
+        node_id = f'{str(id(self))}'
+        graph.node(node_id, f"x_{self.var_index}")
+        if parent_id:
+            graph.edge(parent_id, node_id)
     
 class Value(Terminal):
     value = [None, None]  # первое - значение константы, второе - размерность задачи
@@ -129,5 +159,10 @@ class Value(Terminal):
         self.IsTerminal = 'value'
     def calculate(self, x):
         return self.value[0] * np.ones(self.value[1])
+    def visualize(self, graph, parent_id=None):
+        node_id = f'{str(id(self))}'
+        graph.node(node_id, f"{self.value[0]:.2f}")
+        if parent_id:
+            graph.edge(parent_id, node_id)
 
 
