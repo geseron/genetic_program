75be2622452ddf43c5e24353ac87fa4b7725f4eb
Kozlov Vadim <kozlovvr10@gmail.com>
2024-12-12 21:43:57 +0700

    Починил генерацию и вычисление фитнеса

--- fitness.py
+++ fitness.py
@@ -2,7 +2,10 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 def fitness(rule, y_train, true_class):
-    predicted_class = np.zeros(len(rule))
-    predicted_class[y_train > rule] = 1
+    predicted_class = np.zeros(len(y_train))
+    mask = y_train > rule
+    # if len(mask) == 0:
+    #     return 0
+    predicted_class[mask] = 1
     fit = accuracy_score(true_class, predicted_class)
     return fit
--- population.py
+++ population.py
@@ -3,29 +3,31 @@ import numpy as np
 from fitness import fitness
 
 class gp_population():
-    def __init__(self, X_train, y_train, true_class_labels, pop_size = 5, generations_count=20, mutation_probability = 0.3, depth = 4):
+    def __init__(self, X_train, y_train, true_class_labels, dimension_count, pop_size = 5, generations_count=20, mutation_probability = 0.3, depth = 4):
         self.X_train = X_train
         self.y_train = y_train
         self.class_labels = true_class_labels
         self.pop_size = pop_size
         self.generations_count = generations_count
-        self.dimensions = np.shape(X_train)[1]
+        self.dimensions = dimension_count
         self.mutation_probability = mutation_probability
         self.depth_limit = depth
+        self.population = self.generate_population()
+
 
 
     def generate_population(self):
         population = []
         while len(population) < self.pop_size:
             temp_individ = gp_individ(self.dimensions, self.mutation_probability, depth=self.depth_limit)
-            rule = population[-1].calculate(self.X_train)
+            rule = temp_individ.predict(self.X_train)
+            rule = temp_individ.predict(self.X_train)
             if np.any(rule == None) or np.any(rule == np.inf) or np.any(rule == (-np.inf)):
                 continue
             fitness_value = fitness(rule, self.y_train, self.class_labels)
-            population.append({
-                'individ':temp_individ,
-                'fit_value':fitness_value
-                              })
+            population.append( [temp_individ, fitness_value] )
+        population = np.array(population)
+        return population
             
             
 
