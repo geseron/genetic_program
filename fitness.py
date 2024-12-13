6560c88851295f3256b0820b2eb5a6b622205696
Kozlov Vadim <kozlovvr10@gmail.com>
2024-12-13 11:16:23 +0700

    штраф к фитнесу за глубину дерева. Проверка на None, inf

--- fitness.py
+++ fitness.py
@@ -1,11 +1,18 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-def fitness(rule, y_train, true_class):
+def fitness(rule, y_train, true_class, depth, depth_limit):
     predicted_class = np.zeros(len(y_train))
     mask = y_train > rule
     # if len(mask) == 0:
     #     return 0
     predicted_class[mask] = 1
-    fit = accuracy_score(true_class, predicted_class)
+    penalty = 0
+    if depth>depth_limit:
+        penalty = 0.05 * ( depth - depth_limit )
+    fit = accuracy_score(true_class, predicted_class) - penalty
     return fit
+
+def check_adequacy(rule):
+    # true если есть None, inf
+    return np.any(rule == None) or np.any(rule == np.inf) or np.any(rule == (-np.inf))
