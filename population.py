c783bae7281ef6bad18c2cfb8d9f9aa158f90ed1
Kozlov Vadim <kozlovvr10@gmail.com>
2024-12-13 11:19:41 +0700

    Проверка на  None в rule отдельной функцией. фитнес со штрафами на глубину

--- population.py
+++ population.py
@@ -1,9 +1,13 @@
 from individ import gp_individ
 import numpy as np
-from fitness import fitness
+from fitness import *
+from selection import *
 
 class gp_population():
-    def __init__(self, X_train, y_train, true_class_labels, dimension_count, pop_size = 5, generations_count=20, mutation_probability = 0.3, depth = 4):
+    selection_type = { 'tournament': tournament_selection,
+                        'proportional': proportional_selection
+                     }
+    def __init__(self, X_train, y_train, true_class_labels, dimension_count, selection_type='tournament', pop_size = 5, generations_count=20, mutation_probability = 0.3, depth = 4):
         self.X_train = X_train
         self.y_train = y_train
         self.class_labels = true_class_labels
@@ -12,6 +16,7 @@ class gp_population():
         self.dimensions = dimension_count
         self.mutation_probability = mutation_probability
         self.depth_limit = depth
+        self.selection = self.selection_type[selection_type]
         self.population = self.generate_population()
 
 
@@ -21,10 +26,9 @@ class gp_population():
         while len(population) < self.pop_size:
             temp_individ = gp_individ(self.dimensions, self.mutation_probability, depth=self.depth_limit)
             rule = temp_individ.predict(self.X_train)
-            rule = temp_individ.predict(self.X_train)
-            if np.any(rule == None) or np.any(rule == np.inf) or np.any(rule == (-np.inf)):
+            if check_adequacy(rule): # true если есть None, inf
                 continue
-            fitness_value = fitness(rule, self.y_train, self.class_labels)
+            fitness_value = fitness(rule, self.y_train, self.class_labels, temp_individ.depth, self.depth_limit)
             population.append( [temp_individ, fitness_value] )
         population = np.array(population)
         return population
